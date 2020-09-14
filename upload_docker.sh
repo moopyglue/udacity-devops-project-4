8d8d7a43457cff1a639ecf6fc1fc86584d3ca526
Simon Wall <moopyglue@gmail.com>
2020-09-15 00:22:24 +0100

    added in a couple of missed items to pass the post

--- upload_docker.sh
+++ upload_docker.sh
@@ -4,13 +4,23 @@
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
-# Create dockerpath
-dockerpath=qmkey/udacity:project4
+
+source_tag=sjhw:project4
+destination_user=qmkey
+destination_reg="https://index.docker.io/v1/"
+destination_tag=$destination_user/udacity:project4
+
+echo "Docker Source ID and Image: $source_tag"
+echo "Docker Destination ID and Image: $destination_tag"
 
 # Step 2:  
-# Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+# Tag and Authentication
+
+docker tag $source_tag $destination_tag
+
+credentials="$( docker system info 2>/dev/null | grep -E " (Username|Registry): " | awk '{ printf $2 ":"  }' )" 
+[[ $credentials == "$destination_user:$destination_reg:" ]] || docker login --username $destination_user
 
 # Step 3:
 # Push image to a docker repository
-docker push qmkey/udacity:project4
+docker push $destination_tag
