3e68c0da6c1721f06c9f6360174142add3986d0a
Simon Wall <moopyglue@gmail.com>
2020-09-14 01:01:54 +0000

    first outputs + app.py change

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,11 +4,11 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=sjhw:project4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubctl docker 
 
 # Step 3:
 # List kubernetes pods
